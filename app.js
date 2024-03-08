b961088d00c2abcda5edce64908a19e8943d8372
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-03-08 11:14:11 +0000

    get movies by id done

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 // Import the required modules
 import express from "express";
 
-
-
 // Import your helper functions for your first resource here
-// import {
-//   getResourceOne,
-//   getResourceOneById,
-//   createResourceOne,
-//   updateResourceOneById,
-//   deleteResourceOneById,
-// } from "./resource_one.js";
-
+import { getMovieById } from "./movies.js";
 
 // Import your helper functions for your second resource here
 // import {
@@ -22,8 +13,6 @@ import express from "express";
 //   deleteResourceTwoById,
 // } from "./resource_two.js";
 
-
-
 // Initialize the express app
 const app = express();
 // Retrieve the port number from environment variables
@@ -31,65 +20,60 @@ const PORT = process.env.PORT;
 
 app.use(express.json()); // express.json() middleware is used to parse incoming JSON requests
 
-
-
-
 // Resource One Route Handlers
 
 // Endpoint to retrieve all <resource_one>
 app.get("/resourceone/", async function (req, res) {
-    console.log("I'm alive");
-    res.status(200).send("I'm alive!");
+	console.log("I'm alive");
+	res.status(200).send("I'm alive!");
 });
 
 // Endpoint to retrieve a <resource_one> by id
-app.get("/resourceone/:id", async function (req, res) {
+app.get("/movies/:id", async function (req, res) {
+	// get id from request
+	// call helper function
+	// respond with data
+	const id = req.params.id;
+	const movie = await getMovieById(id);
+	if (!movie) {
+		return res
+			.status(404)
+			.json({ status: "fail", data: { msg: "Movie not found" } });
+	}
+
+	res.status(200).json({ status: "success", data: movie });
 });
 
 // Endpoint to create a new <resource_one>
-app.post("/resourceone/", async function (req, res) {
-});
+app.post("/resourceone/", async function (req, res) {});
 
 // Endpoint to update a specific <resource_one> by id
-app.patch("/resourceone/:id", async function (req, res) {
-});
+app.patch("/resourceone/:id", async function (req, res) {});
 
 // Endpoint to delete a specific <resource_one> by id
-app.delete("/resourceone/:id", async function (req, res) {
-});
-
-
-
+app.delete("/resourceone/:id", async function (req, res) {});
 
 // Resource Two Route Handlers
 
 // Endpoint to retrieve all <resource_twos>
 app.get("/resourcetwo/", async function (req, res) {
-    const authors = await getAuthors();
-    res.status(200).json({ status: "success", data: authors });
-  });
-  
-  // Endpoint to retrieve a <resource_twos> by id
-  app.get("/resourcetwo/:id", async function (req, res) {
-  });
-  
-  // Endpoint to create a new <resource_twos>
-  app.post("/resourcetwo/", async function (req, res) {
-  });
-  
-  // Endpoint to update a specific <resource_twos> by id
-  app.patch("/resourcetwo/:id", async function (req, res) {
-  });
-  
-  // Endpoint to delete a specific <resource_twos> by id
-  app.delete("/resourcetwo/:id", async function (req, res) {
-  });
+	const authors = await getAuthors();
+	res.status(200).json({ status: "success", data: authors });
+});
 
+// Endpoint to retrieve a <resource_twos> by id
+app.get("/resourcetwo/:id", async function (req, res) {});
 
+// Endpoint to create a new <resource_twos>
+app.post("/resourcetwo/", async function (req, res) {});
 
+// Endpoint to update a specific <resource_twos> by id
+app.patch("/resourcetwo/:id", async function (req, res) {});
 
+// Endpoint to delete a specific <resource_twos> by id
+app.delete("/resourcetwo/:id", async function (req, res) {});
 
 // Start the server and listen on the specified port
 app.listen(PORT, function () {
-  console.log(`Server listening on port ${PORT}`);
+	console.log(`Server listening on port ${PORT}`);
 });
--- movies.js
+++ movies.js
@@ -2,26 +2,30 @@
 import { pool } from "./db/index.js";
 
 export async function getResourceOne() {
-  try {
-    const queryText = "SELECT * FROM movies ORDER BY id";
-    const movies = await pool.query(queryText);
-    return movies.rows;
-  } catch (error) {}
-  // Query the database and return all resource ones
+	try {
+		const queryText = "SELECT * FROM movies ORDER BY id";
+		const movies = await pool.query(queryText);
+		return movies.rows;
+	} catch (error) {}
+	// Query the database and return all resource ones
 }
 
-export async function getResourceOneById(id) {
-  // Query the database and return the resource with a matching id or null
+export async function getMovieById(id) {
+	// get id from request
+	// Query the database and return the resource with a matching id or null
+	const queryText = "SELECT * FROM movies WHERE movies.id = $1";
+	const movie = await pool.query(queryText, [id]);
+	return movie.rows[0] || null;
 }
 
 export async function createResourceOne(resource) {
-  // Query the database to create an resource and return the newly created resource
+	// Query the database to create an resource and return the newly created resource
 }
 
 export async function updateResourceOneById(id, updates) {
-  // Query the database to update the resource and return the newly updated resource or null
+	// Query the database to update the resource and return the newly updated resource or null
 }
 
 export async function deleteResourceOneById(id) {
-  // Query the database to delete the resource and return the deleted resource or null
+	// Query the database to delete the resource and return the deleted resource or null
 }
