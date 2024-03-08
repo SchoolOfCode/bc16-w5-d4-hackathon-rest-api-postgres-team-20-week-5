ab486f6d7eed38593eb8c677f2ad7da3b763ca0a
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-03-08 10:32:07 +0000

    Task 2 SQL script done

--- db/scripts/reset-database.js
+++ db/scripts/reset-database.js
@@ -1,63 +1,63 @@
 import { pool } from "../index.js";
 
-
-
 // >>> MAKE SURE YOU UNDERSTAND THIS FILE AND WHAT IT'S DOING <<<
 // >>> FEEL FREE TO CHANGE IT TO MAKE YOUR OWN RESOURCES (TABLES AND PROPERTIES) - YOU DON'T HAVE TO USE ALBUMS AND ARTISTS <<<
 
-
-
 async function resetDatabase() {
-  try {
-    // Drop existing tables if they exist
-    await pool.query(`
-        DROP TABLE IF EXISTS artists CASCADE;
-        DROP TABLE IF EXISTS albums CASCADE;
+	try {
+		// Drop existing tables if they exist
+		await pool.query(`
+        DROP TABLE IF EXISTS movies CASCADE;
+        DROP TABLE IF EXISTS directors CASCADE;
     `);
 
-    // Create the artists table
-    await pool.query(`
-        CREATE TABLE artists (
-            id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-            name VARCHAR(255) NOT NULL
-        );
-    `);
+		await pool.query(`
+    CREATE TABLE directors (
+        id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        date_of_birth DATE,
+        country VARCHAR(255) NOT NULL
+    );
+`);
 
-    // Create the albums table with a foreign key to the artists table
-    await pool.query(`
-        CREATE TABLE albums (
+		// Create the artists table
+		await pool.query(`
+        CREATE TABLE movies (
             id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
             title VARCHAR(255) NOT NULL,
-            published_date DATE,
-            artist_id INT REFERENCES artists(id)
+            release_date DATE,
+            rating INT NOT NULL,
+            director_id INT REFERENCES directors(id)
         );
     `);
 
-    // Seed the artists table
-    await pool.query(`
-        INSERT INTO artists (name)
+		// Create the albums table with a foreign key to the artists table
+
+		// Seed the artists table
+		await pool.query(`
+        INSERT INTO directors (name, date_of_birth, country)
         VALUES 
-            ('Dua Lipa'),
-            ('Jay-Z');
+            ('Dua Lipa', '1930-06-26', 'Argentina'),
+            ('Jay-Z', '1998-07-02', 'USA');
     `);
 
-    // Seed the albums table
-    await pool.query(`
-        INSERT INTO albums (title, published_date, artist_id)
+		// Seed the albums table
+		await pool.query(`
+        INSERT INTO movies (title, release_date, rating, director_id)
         VALUES 
-            ('Dua Lipa', '2017-06-02', 1),
-            ('Future Nostalgia', '2020-03-27', 1),
-            ('Reasonable Doubt', '1996-06-25', 2),
-            ('The Blueprint', '2001-09-11', 2);
+            ('Zombie Blood Bath', '2017-06-02', 4, 1),
+            ('Night of the Living Dead', '2020-03-27', 5, 1),
+            ('Reasonable Doubt', '1996-06-25', 3, 2),
+            ('The Blueprint', '2001-09-11', 4, 2);
     `);
 
-    console.log("Database reset successful");
-  } catch (error) {
-    console.error("Database reset failed: ", error);
-  } finally {
-    // End the pool
-    await pool.end();
-  }
+		console.log("Database reset successful");
+	} catch (error) {
+		console.error("Database reset failed: ", error);
+	} finally {
+		// End the pool
+		await pool.end();
+	}
 }
 
 await resetDatabase();
