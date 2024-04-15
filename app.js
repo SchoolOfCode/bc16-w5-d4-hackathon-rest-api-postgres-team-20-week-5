198263bdaa2c14193737730da45f85591d86d991
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-04-15 14:04:37 +0100

    Update app.js
    sdfjhvhsd

--- app.js
+++ app.js
@@ -23,45 +23,45 @@ app.use(express.json()); // express.json() middleware is used to parse incoming 
 // Resource One Route Handlers
 
 app.get("/movies/", async function (req, res) {
-  try {
-    const movies = await getMovies();
-    res.status(200).json({ success: true, payload: movies });
-  } catch (error) {
-    console.error(error);
-    res.status(404).send("No such directory");
-  }
+	try {
+		const movies = await getMovies();
+		res.status(200).json({ success: true, payload: movies });
+	} catch (error) {
+		console.error(error);
+		res.status(404).send("No such directory");
+	}
 });
 
 // Endpoint to retrieve a <resource_one> by id
 app.get("/movies/:id", async function (req, res) {
-  // get id from request
-  // call helper function
-  // respond with data
-  const id = req.params.id;
-  const movie = await getMovieById(id);
-  if (!movie) {
-    return res
-      .status(404)
-      .json({ status: "fail", data: { msg: "Movie not found" } });
-  }
+	// get id from request
+	// call helper function
+	// respond with data
+	const id = req.params.id;
+	const movie = await getMovieById(id);
+	if (!movie) {
+		return res
+			.status(404)
+			.json({ status: "fail", data: { msg: "Movie not found" } });
+	}
 
-  res.status(200).json({ status: "success", data: movie });
+	res.status(200).json({ status: "success", data: movie });
 });
 
 // Endpoint to create a new <resource_one>
 app.post("/movies/", async function (req, res) {
-  try {
-    const movieData = req.body;
-    const addedMovie = await createMovie(movieData);
-    res.status(201).json({ success: true, payload: addedMovie });
-  } catch (error) {
-    console.log(error);
-    return "error";
-  }
+	try {
+		const movieData = req.body;
+		const addedMovie = await createMovie(movieData);
+		res.status(201).json({ success: true, payload: addedMovie });
+	} catch (error) {
+		console.log(error);
+		return "error";
+	}
 });
 
 // Endpoint to update a specific <resource_one> by id
-app.patch("/resourceone/:id", async function (req, res) {});
+app.patch("/movies/:id", async function (req, res) {});
 
 // Endpoint to delete a specific <resource_one> by id
 app.delete("/resourceone/:id", async function (req, res) {});
@@ -70,8 +70,8 @@ app.delete("/resourceone/:id", async function (req, res) {});
 
 // Endpoint to retrieve all <resource_twos>
 app.get("/resourcetwo/", async function (req, res) {
-  const authors = await getAuthors();
-  res.status(200).json({ status: "success", data: authors });
+	const authors = await getAuthors();
+	res.status(200).json({ status: "success", data: authors });
 });
 
 // Endpoint to retrieve a <resource_twos> by id
@@ -88,5 +88,5 @@ app.delete("/resourcetwo/:id", async function (req, res) {});
 
 // Start the server and listen on the specified port
 app.listen(PORT, function () {
-  console.log(`Server listening on port ${PORT}`);
+	console.log(`Server listening on port ${PORT}`);
 });
